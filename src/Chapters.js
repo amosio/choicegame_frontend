ed24bd32b13e1bcfd1a2b3c619c339586e45aec8
amosio <arkadiuszmosio@gmail.com>
2019-11-23 12:15:11 +0100

    v 1.1.
    add react transition

--- src/Chapters.js
+++ src/Chapters.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Chapter from './Chapter';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import './chapters-transition.css';
 
 function Chapters(props) {
-  return props.chaptersData.map((chapter, i) => (
-    <Chapter
-      chapterData={chapter}
-      handleChoice={props.handleChoice}
-      chapterId={i}
-      key={chapter.id}
-    />
-  ));
+  return (
+    <TransitionGroup className='chapters-list'>
+      {props.chaptersData.map((chapter, i) => (
+        <CSSTransition key={chapter.id} timeout={500} classNames="fade">
+          <Chapter
+            chapterData={chapter}
+            handleChoice={props.handleChoice}
+            chapterId={i}
+            key={chapter.id}
+          />
+        </CSSTransition>))}
+    </TransitionGroup>
+  );
 }
 
 export default Chapters;
